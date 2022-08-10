866233f24be8422a68aff5ef0752c20a6a62b463
Karen <zoroglian.karen@gmail.com>
2022-08-10 14:55:34 +0400

    added unit test

--- database.test.js
+++ database.test.js
@@ -10,10 +10,16 @@ test("response should return array", async () => {
 
   const result = response.result;
 
-  
-
   expect(Array.isArray(result)).toBe(true);
+  result.forEach((element) => {
+    const hasId = element.hasOwnProperty("id");
+    expect(hasId).toBe(true);
+  });
 
+  result.forEach((element) => {
+    const hasTitle = element.hasOwnProperty("title");
+    expect(hasTitle).toBe(true);
+  });
   // response has status code 200
   // response has todos as array
 });
